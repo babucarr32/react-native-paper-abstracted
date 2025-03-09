1318aa1e0f4c45cb1336bd13723fef1b7bd58df0
babucarr32 <baboucarry@gmail.com>
2025-03-09 13:02:56 +0000

    debug:file not found on prod

--- apps/web/actions.ts
+++ apps/web/actions.ts
@@ -5,7 +5,16 @@ import { highlightCode } from "./libs";
 
 export async function getContent(filePath: string): Promise<{ raw: string; content: string }> {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(process.cwd(), filePath), { encoding: "utf8" }, async (err, data) => {
+    const absolutePath = process.env.NODE_ENV === "production"
+      ? path.join("/vercel/path0/apps/web", filePath)
+      : path.join(process.cwd(), filePath);
+
+    console.log("VERCEL ABSOLUTE", fs.readdir(absolutePath));
+    console.log("VERCEL PATH0", fs.readdir("/vercel/path0"));
+    console.log("VERCEL APPS", fs.readdir("/vercel/path0/apps"));
+    console.log("VERCEL WEB", fs.readdir("/vercel/path0/apps/web"));
+
+    fs.readFile(absolutePath, { encoding: "utf8" }, async (err, data) => {
       if (err) {
         reject(err);
       }
